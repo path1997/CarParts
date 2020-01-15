3d5e6ee2e48d6a8c16a2cccae88d4e0a99c2e8e5
Mateusz <mateeeuszek97@gmail.com>
2020-01-15 22:55:07 +0100

    mapka

--- app/src/main/java/com/example/carparts/Geolocation.java
+++ app/src/main/java/com/example/carparts/Geolocation.java
@@ -3,7 +3,9 @@ package com.example.carparts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -12,6 +14,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
@@ -50,6 +53,38 @@ public class Geolocation extends FragmentActivity implements GoogleMap.OnMyLocat
         LatLng carparts = new LatLng(51.7767068, 19.488359);
         mMap.addMarker(new MarkerOptions().position(carparts).title("Carparts shop"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(carparts));
+
+        LatLng intercars = new LatLng(51.8054863, 19.4195499);
+        mMap.addMarker(new MarkerOptions().position(intercars).title("InterCars").snippet("intercars.pl"));
+
+        LatLng iparts = new LatLng(51.7843386,19.4572096);
+        mMap.addMarker(new MarkerOptions().position(iparts).title("IParts").snippet("iparts.pl"));
+
+        LatLng ucando = new LatLng(51.796535, 19.3851062);
+        mMap.addMarker(new MarkerOptions().position(ucando).title("Ucando").snippet("ucando.pl"));
+
+        LatLng autocraft24 = new LatLng(51.7842983, 19.4511713);
+        mMap.addMarker(new MarkerOptions().position(autocraft24).title("Autocraft24").snippet("autocraft24.pl"));
+
+
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+             @Override
+             public void onInfoWindowClick(Marker marker) {
+                 if(marker != null && marker.getTitle().equals("InterCars")){
+                     goToUrl ( "https://intercars.pl/");
+                 }
+                 if(marker != null && marker.getTitle().equals("IParts")){
+                     goToUrl ( "https://www.iparts.pl/");
+                 }
+                 if(marker != null && marker.getTitle().equals("Ucando")){
+                     goToUrl ( "https://www.ucando.pl/");
+                 }
+                 if(marker != null && marker.getTitle().equals("Autocraft24")){
+                     goToUrl ( "https://autocraft24.pl/");
+                 }
+             }
+         });
+
         mMap.setMyLocationEnabled(true);
         mMap.setOnMyLocationButtonClickListener(this);
         mMap.setOnMyLocationClickListener(this);
@@ -61,6 +96,12 @@ public class Geolocation extends FragmentActivity implements GoogleMap.OnMyLocat
         mUiSettings.setScrollGesturesEnabled(true);
         mUiSettings.setTiltGesturesEnabled(true);
     }
+    private void goToUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
 
     @Override
     public boolean onMyLocationButtonClick() {
